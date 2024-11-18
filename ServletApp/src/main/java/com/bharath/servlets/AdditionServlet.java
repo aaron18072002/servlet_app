13c786ceb7cf9ec63b88cd44caef2344ad5f039a
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-18 15:33:40 +0700

    updated

--- ServletApp/src/main/java/com/bharath/servlets/AdditionServlet.java
+++ ServletApp/src/main/java/com/bharath/servlets/AdditionServlet.java
@@ -14,9 +14,9 @@ public class AdditionServlet extends GenericServlet {
 	public void service(ServletRequest request, ServletResponse response) 
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		if(request.getParameter("number1") == null && request.getParameter("number2") == null) {
-			Long num1 = Long.valueOf(request.getParameter("number1"));
-			Long num2 = Long.valueOf(request.getParameter("number2"));
+		if(request.getParameter("num1") != null && request.getParameter("num2") != null) {
+			Long num1 = Long.valueOf(request.getParameter("num1"));
+			Long num2 = Long.valueOf(request.getParameter("num2"));
 			
 			Long result = num1 + num2;
 			
