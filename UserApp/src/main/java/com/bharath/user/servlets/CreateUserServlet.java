0ea4f75a71a456af6e1afe25a03bdfe02aefde31
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-19 18:23:36 +0700

    updated

--- UserApp/src/main/java/com/bharath/user/servlets/CreateUserServlet.java
+++ UserApp/src/main/java/com/bharath/user/servlets/CreateUserServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -42,9 +43,28 @@ public class CreateUserServlet extends HttpServlet {
 	}
 	
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
 			throws ServletException, IOException {		
-		super.doPost(req, resp);
+		String firstName = req.getParameter("firstName");
+		String lastName = req.getParameter("firstName");
+		String password = req.getParameter("password");
+		String email = req.getParameter("email");
+		try(Statement statement = this.connection.createStatement()) {
+			int result = statement.executeUpdate
+					("""
+						INSERT INTO USER(FirstName,LastName,Email,Password)
+						VALUES ('%s','%s','%s','%s');
+							""".formatted(firstName,lastName,email,password));
+			
+			PrintWriter out = res.getWriter();
+			if(result > 0) {				
+				out.printf("<h1>%d user have inserted</h1>", result).println();
+			} else {
+				out.println("<h1>Error while inserting user</h1>");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
