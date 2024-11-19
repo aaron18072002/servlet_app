b14cecb67435b72cad16c03a2d14b884d60cd033
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-19 11:31:56 +0700

    updated

--- JDBCBasics/src/com/bharath/jdbc/dao/AccountDAO.java
+++ JDBCBasics/src/com/bharath/jdbc/dao/AccountDAO.java
@@ -3,13 +3,31 @@ package com.bharath.jdbc.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AccountDAO {
 	public static void main(String[] args) {
 		try {
 			Connection connection = DriverManager.getConnection
 					("jdbc:mysql://localhost/mysql","root","123456");
-			System.out.println(connection.toString());
+			Statement statement = connection.createStatement();
+//			int result = statement.executeUpdate
+//					("""
+//						INSERT INTO ACCOUNT(accno,lastname,firstname,bal)
+//						VALUES (2,'Nguyen','Nemo',15000);
+//							""");
+//			System.out.printf("%d rows affected", result).println();
+			int result = statement.executeUpdate
+					("""
+						SET @currentBalOfNemo = (SELECT bal FROM ACCOUNT WHERE accno = 2);
+
+						SET SQL_SAFE_UPDATES = 0; 
+						UPDATE ACCOUNT
+						SET bal = @currentBalOfNemo + 35000
+						WHERE accno = 2;
+						SET SQL_SAFE_UPDATES = 1;
+							""");
+			System.out.printf("%d rows affected", result).println();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
