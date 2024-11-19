95db8f6c8b39c2858703f59799b634b95297a7ee
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-19 11:43:06 +0700

    updated

--- JDBCBasics/src/com/bharath/jdbc/dao/AccountDAO.java
+++ JDBCBasics/src/com/bharath/jdbc/dao/AccountDAO.java
@@ -11,23 +11,32 @@ public class AccountDAO {
 			Connection connection = DriverManager.getConnection
 					("jdbc:mysql://localhost/mysql","root","123456");
 			Statement statement = connection.createStatement();
+			
+			int insertedResult = statement.executeUpdate
+					("""
+						INSERT INTO ACCOUNT(accno,lastname,firstname,bal)
+						VALUES (3,'Lebron','James',12000);
+							""");
+			System.out.printf("%d rows affected", insertedResult).println();
+			
+			int deletedResult = statement.executeUpdate
+					(""" 
+						DELETE FROM ACCOUNT
+						WHERE accno = 3;	
+							""");
+			System.out.printf("%d rows affected", deletedResult).println();
+			
 //			int result = statement.executeUpdate
 //					("""
-//						INSERT INTO ACCOUNT(accno,lastname,firstname,bal)
-//						VALUES (2,'Nguyen','Nemo',15000);
+//						SET @currentBalOfNemo = (SELECT bal FROM ACCOUNT WHERE accno = 2);
+//
+//						SET SQL_SAFE_UPDATES = 0; 
+//						UPDATE ACCOUNT
+//						SET bal = @currentBalOfNemo + 35000
+//						WHERE accno = 2;
+//						SET SQL_SAFE_UPDATES = 1;
 //							""");
 //			System.out.printf("%d rows affected", result).println();
-			int result = statement.executeUpdate
-					("""
-						SET @currentBalOfNemo = (SELECT bal FROM ACCOUNT WHERE accno = 2);
-
-						SET SQL_SAFE_UPDATES = 0; 
-						UPDATE ACCOUNT
-						SET bal = @currentBalOfNemo + 35000
-						WHERE accno = 2;
-						SET SQL_SAFE_UPDATES = 1;
-							""");
-			System.out.printf("%d rows affected", result).println();
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
