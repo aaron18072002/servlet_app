399138fa33fd5d6b482504df3c0e26a573414a59
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-19 17:06:17 +0700

    updated

--- UserApp/src/main/java/com/bharath/user/servlets/CreateUserServlet.java
+++ UserApp/src/main/java/com/bharath/user/servlets/CreateUserServlet.java
@@ -1,6 +1,9 @@
 package com.bharath.user.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,10 +14,17 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/addServlet")
 public class CreateUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private Connection connection;
 	
 	@Override
 	public void init() throws ServletException {		
 		super.init();
+		try {
+			this.connection = DriverManager.getConnection
+					("jdbc:mysql://localhost/servlet_app", "root", "123456");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -26,5 +36,12 @@ public class CreateUserServlet extends HttpServlet {
 	@Override
 	public void destroy() {
 		super.destroy();
+		try {
+			if(this.connection != null) {				
+				this.connection.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
