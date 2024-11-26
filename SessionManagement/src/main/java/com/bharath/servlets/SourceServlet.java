95a95f44577a6f2796b19a6e7c22bcc9b1242fc8
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-11-26 10:08:46 +0700

    updated

--- SessionManagement/src/main/java/com/bharath/servlets/SourceServlet.java
+++ SessionManagement/src/main/java/com/bharath/servlets/SourceServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,11 +17,20 @@ public class SourceServlet extends HttpServlet {
        
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		String userName= request.getParameter("username");
+		Cookie[] cookies = request.getCookies();
+		if(cookies.length > 0 && cookies != null)
+		for(Cookie cookie:cookies) {
+			System.out.println(cookie.getName());
+			System.out.println(cookie.getValue());
+		}
+		response.addCookie(new Cookie("accessToken", "123456"));
+		String userName= request.getParameter("userName");
 		HttpSession session = request.getSession();
 		session.setAttribute("user", userName);
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+		//URL rewriting
+		String url = "targetServlet?assessToken=123456";
 		out.println("<a href='targetServlet'>Click here to get username</a>");
 	}
 
--- SessionManagement/src/main/java/com/bharath/servlets/TargetServlet.java
+++ SessionManagement/src/main/java/com/bharath/servlets/TargetServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +18,11 @@ public class TargetServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 			throws ServletException, IOException {
+		Cookie[] cookies = req.getCookies();
+		for(Cookie cookie:cookies) {
+			System.out.println(cookie.getName());
+			System.out.println(cookie.getValue());
+		}
 		HttpSession session = req.getSession();
 		String userName = (String)session.getAttribute("user");
 		PrintWriter out = resp.getWriter();
